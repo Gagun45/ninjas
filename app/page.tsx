95c0f2d8a0cacf2259fc2f738b17cecc503b0b1b
Gagun45 <selyanchyn45@gmail.com>
2025-09-22 18:51:25 +0300

    prisma init setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <section>homepage</section>;
+  return (
+    <section>
+      <h1>Homepage</h1>
+      <div>All heroes section</div>
+    </section>
+  );
 }
